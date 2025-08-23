a1cdfa39507f5641e972f682df75ad56f0057f33
Andrew3369 <andrewjanosbabos@gmail.com>
2025-08-23 16:14:40 -0400

    Updated Display methods
    Now both cards are the center of the window

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,11 +6,8 @@
 #include <QGraphicsView>
 #include <qgraphicsitem.h>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow)
 {
-
     //initializations
     deck = new Deck();
     //player = new Player();
@@ -24,27 +21,27 @@ MainWindow::MainWindow(QWidget *parent)
 
 
     ui->setupUi(this);
-    QWidget *central = new QWidget(this);
-    setCentralWidget(central);
-
-    QLabel *label = new QLabel(central);
     QGraphicsScene *scene = new QGraphicsScene(this);
     QGraphicsView *view = new QGraphicsView(scene, this);
+    setCentralWidget(view);
 
+    //QLabel *label = new QLabel(central);
+
+    //FILENAMES of cards
     QString cardFilename1 = ":/assets/" + hand[0].toFilename();
     QString cardFilename2 = ":/assets/" + hand[1].toFilename();
 
     QPixmap card1(cardFilename1);
     QGraphicsPixmapItem* cardItem1 = scene->addPixmap(card1);
+    cardItem1->setPos(100, 100);
 
     QPixmap card2(cardFilename2);
     QGraphicsPixmapItem* cardItem2 = scene->addPixmap(card2);
+    cardItem2->setPos(175, 100);
 
     if (card1.isNull() && card2.isNull())
         qDebug() << "failed loading image: " + cardFilename1 + " and " + cardFilename2;
 
-    cardItem1->setPos(100, 500);
-    cardItem2->setPos(400, 500);
 
     //view->show();
 }
